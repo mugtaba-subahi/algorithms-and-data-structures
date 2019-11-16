b19c0316756b514c2e76ae6e24de165ee3f83b8f
mugtaba-subahi <m_subahi@hotmail.com>
2019-11-16 14:05:48 +0000

    commented code

--- LinkedLists.js
+++ LinkedLists.js
@@ -1,20 +1,33 @@
-class LinkedList {}
-
 class Node {
-  constructor(data, name) {
+  constructor(data) {
     this.data = data;
-    this.name = name;
     this.next = null;
   }
 }
 
-const nodeA = new Node(6, 'A');
-const nodeB = new Node(3, 'B');
-const nodeC = new Node(1, 'C');
+// manually create nodes
+const nodeA = new Node(6);
+const nodeB = new Node(3);
+const nodeC = new Node(1);
+const nodeD = new Node(4);
+const nodeE = new Node(5);
 
+// manually link nodes
 nodeA.next = nodeB;
 nodeB.next = nodeC;
+nodeC.next = nodeD;
+nodeD.next = nodeE;
 
-console.log(nodeB);
+let counter = 0;
 
-// test
+// count number of nodes in a linked-list recusively
+const countNode = node => {
+  if (!node.next) return ++counter;
+
+  ++counter;
+  countNode(node.next);
+};
+
+countNode(nodeA);
+
+console.log(`There are ${counter} nodes in the linked list`);
